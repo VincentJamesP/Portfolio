fee0b4d5cc318d1661d252aece1a4b60e4365fc7
Vincent James Demetria <vincent.demetria@kationtechnologies.com>
2024-01-04 01:26:27 +0800

    Add: clear contact form input after successful email sending

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -9,7 +9,24 @@ import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
 
 const Contact = () => {
-  const { ref } = useSectionInView("Contact");
+  const { ref } = useSectionInView("Contact"); 
+
+  const [formData, setFormData] = useState({
+    senderEmail: "",
+    message: "",
+  });
+
+  const handleInputChange = (e: { target: { name: any; value: any; }; }) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const resetForm = () => {
+    setFormData({
+      senderEmail: "",
+      message: "",
+    });
+  };
 
   return (
     <motion.section
@@ -33,7 +50,7 @@ const Contact = () => {
       <form 
         className="mt-10 flex flex-col dark:text-black" 
         action={async (formData) => {
-            const { data, error } = await sendEmail(formData);
+            const { error } = await sendEmail(formData);
 
             if (error) {
               toast.error(error);
@@ -41,12 +58,15 @@ const Contact = () => {
             }
 
             toast.success("Email sent successfully!");
+            resetForm();
         }}
       >
         <input
           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
+          value={formData.senderEmail}
+          onChange={handleInputChange}
           required
           maxLength={500}
           placeholder="Your email"
@@ -54,6 +74,8 @@ const Contact = () => {
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
+          value={formData.message}
+          onChange={handleInputChange}
           placeholder="Your message"
           required
           maxLength={5000}
